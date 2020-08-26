7aa3e6ad8107f6db6213fd88021575c2e82cab96
Hernan <hernandmf@gmail.com>
2020-08-26 14:11:59 -0500

    We commented the code to explain it

--- Game.js
+++ Game.js
@@ -1,57 +1,82 @@
-let card = document.querySelectorAll(".card");
-let cover = document.querySelectorAll(".cover");
+let card = document.querySelectorAll(".card"); // optenemos todos elementos del DOM con la etiqueta "card"
+let cover = document.querySelectorAll(".cover"); // optenemos todos elementos del DOM con la etiqueta "cover"
 
+/*
+ *Voltea la carta del reverso al frente
+ */
 function voltearTarjeta(index, cardValue, img) {
-  card[index].style.transform = "rotateY(-180deg)";
+  //(posicion, valor, imagen)
+  card[index].style.transform = "rotateY(-180deg)"; // Rota un elemento del array de cards
   // console.log(cover);
 
   setTimeout(() => {
-    card[index].innerHTML = null;
-    cover[index].style.display = "block";
-    console.log(index);
-    card[index].appendChild(cover[index]);
-  }, 300);
+    // restrazamos un poco las instrucciones durmiendo el programa
+    card[index].innerHTML = null; // borramos el contenido de la carta
+    cover[index].style.display = "block"; // mostramos el lado de frente de la imagen
+    // console.log(index);
+    card[index].appendChild(cover[index]); // ingresamos en el elemento card la parte del frente de la carta(cover)
+  }, 300); //milisegundos que se durme el programa
 
-  comparation(index, cardValue, img);
+  comparation(index, cardValue, img); // funcion que se llama para comparar cartas
 }
 
-let question = document.querySelectorAll(".question");
-let back = document.querySelectorAll(".back");
-
+let question = document.querySelectorAll(".question"); // optenemos todos elementos del DOM con la etiqueta "question" que son los signos de interrogacion
+let back = document.querySelectorAll(".back"); // optenemos todos elementos del DOM con la etiqueta "back" son el reverso de la tarjeta
+/*
+ *Voltea la carta del frente al reves
+ */
 function volveravoltear(index) {
-  card[index].style.transform = "rotateY(180deg)";
+  card[index].style.transform = "rotateY(180deg)"; // Rota un elemento del array de cards
   // console.log(back);
 
   setTimeout(() => {
-    card[index].innerHTML = null;
-    back[index].style.display = "block";
+    // restrazamos un poco las instrucciones durmiendo el programa
+    card[index].innerHTML = null; //borramos lo que esta dentro de card
+    back[index].style.display = "block"; // aparecemos el reverso de la carta
     console.log(index);
-    card[index].appendChild(back[index]);
-  }, 300);
+    card[index].appendChild(back[index]); //ingresamos el reverso de la carta en el elemento card
+  }, 300); //milisegundos
 }
 
-let clickEnCarta = 0;
+let clickEnCarta = 0; // esta variable manejara el click en la primera y la segunda carta como un switche
+//----------- estas variables recibiran el valor de la primera carta clickeada la imagen y la posicion de donde se encuentra
+let valueCarta1 = 0;
+let image1 = "";
+let index1 = 0;
+//----------- estas variables recibiran el valor de la segunda carta clickeada la imagen y la posicion de donde se encuentra
+let valueCarta2 = 0;
+let image2 = "";
+let index2 = 0;
+
+/*
+ *Compara las cartas en base a su posición, imagen y valor numerico.
+ */
+
 function comparation(index, cardValue, img) {
   if (clickEnCarta == 0) {
-    let valueCarta1 = cardValue;
-    let image1 = img;
-    let index1 = index;
-    alert(valueCarta1);
+    // si es la primera carta clickeada
+    // almacena los datos de la primera carta
+    valueCarta1 = cardValue;
+    image1 = img;
+    index1 = index;
 
     clickEnCarta = 1;
   } else if (clickEnCarta == 1) {
-    let valueCarta2 = cardValue;
-    let image2 = img;
-    let index2 = index;
+    // si es la segunda carta clickeada
+    // almacena los datos de la segunda carta
+    valueCarta2 = cardValue;
+    image2 = img;
+    index2 = index;
 
-    alert(valueCarta1 + " var2: " + valueCarta2);
-    alert(valueCarta2);
-
-    // if (valueCarta1 == valueCarta2 && image1 == image2) {
-    // } else {
-    //   volveravoltear(index1);
-    //   volveravoltear(index2);
-    // }
+    setTimeout(() => {
+      if (valueCarta1 == valueCarta2 && image1 == image2) {
+        //Se compara carta 1 con carta 2
+      } else {
+        // Si las cartas son distintas se voltean de regreso
+        volveravoltear(index1);
+        volveravoltear(index2);
+      }
+    }, 1000);
 
     clickEnCarta = 0;
   }
